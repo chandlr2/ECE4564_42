d6a8baa27570eec9421275214458ed92746a4c1d
Eric Chandler <chandlr2@vt.edu>
2017-10-07 19:31:37 -0400

    7

--- client.py
+++ client.py
@@ -1,41 +1,71 @@
+import subprocess
 from keys import ckey,csecret,asecret,atoken
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import socket
 import sys
-
-
-host = sys.argv[1]
-#print(host)
-port = 5803
-size = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
 from keys import ckey,csecret,asecret,atoken
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 #consumer key, consumer secret, access token, access secret.
+
+# https://gist.github.com/didler/2395703
+def getopts(argv):
+	opts ={}
+	while sys.argv:
+		if sys.argv[0][0] == '-':
+			opts[sys.argv[0]] = sys.argv[1]
+		sys.argv = sys.argv[1:]
+	return opts
+
+myargs = getopts(sys.argv)
+if not myargs:
+	print('No arguments found')
+	sys.exit()
+if '-p' in myargs:
+	server_port = int(myargs['-p'])
+	myargs.pop('-p', None)
+if '-t' in myargs:
+	hashtag = int(myargs['-b'])
+	myargs.pop('-b', None)
+if '-z' in myargs:
+	socket_size = myargs['-z']
+	myargs.pop('-z', None)
+if '-s' in myargs:
+	host = myargs['-z']
+	myargs.pop('-z', None)
+if len(myargs.keys()) != 0:
+	print('Invalid arguments')
+	sys.exit()
 
 
 
 class listener(StreamListener):
-
+	print('Listening for tweets that contain: ',hashtag)
 	def on_data(self, data):
-		hashtag = "#ECE4564T31"
+		user = data.split('"screen_name":"')[1].split('","')[0]
 		tweet = data.split(',"text":"')[1].split('","source')[0]
+		print('New Tweet: ',tweet,' | User: ',user)
+		parsed = tweet.replace(" ", "_")
+		cmd_beg = 'espeak -ven+f3 -k5 -s150 '
+		with open(os.devnull, 'w') as devnull:
+			subprocess.run(cmd_beg + parsed, stdout=devnull, stderr=devnull, shell=True)
 		if hashtag in tweet:
 			tweet = tweet.replace(hashtag,"")
-			s.connect((host, port))
+			s.connect((host, server_port))
+			print('Conncting to server ',host,' on port',server_port)
 			s.send(tweet.encode())
-			data_ = s.recv(size)
+			print('Sending question: ',tweet)
+			data_ = s.recv(socket_size)
 			s.close()
 			print('Received:', data_)
-			print(tweet)
+			#print(tweet)
 		return(True)
 	def on_error(self, status):
 		print(status)
@@ -44,4 +74,4 @@ auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["#ECE4564T31"])
+twitterStream.filter(track=[hashtag])
