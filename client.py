89508e8745901f848a5228449cf7dc20658c9409
Eric Chandler <chandlr2@vt.edu>
2017-10-07 19:53:21 -0400

    30

--- client.py
+++ client.py
@@ -54,9 +54,9 @@ class listener(StreamListener):
 		tweet = data.split(',"text":"')[1].split('","source')[0]
 		print('New Tweet: ',tweet,' | User: ',user)
 		parsed = tweet.replace(" ", "_")
-		cmd_beg = 'espeak -ven+f3 -k5 -s150 '
-		with open(os.devnull, 'w') as devnull:
-			subprocess.run(cmd_beg + parsed, stdout=devnull, stderr=devnull, shell=True)
+		#cmd_beg = 'espeak -ven+f3 -k5 -s150 '
+		#with open(os.devnull, 'w') as devnull:
+		#	subprocess.run(cmd_beg + parsed, stdout=devnull, stderr=devnull, shell=True)
 		if hashtag in tweet:
 			tweet = tweet.replace(hashtag,"")
 			s.connect((host, server_port))
