572194d4acb0653f8007d3a23a63b95e0cb05b58
Eric Chandler <chandlr2@vt.edu>
2017-10-08 14:58:51 -0400

    Commented

--- client.py
+++ client.py
@@ -1,6 +1,13 @@
+#The following imported libraries are used for both Twitter API, ESPEAK, and Client initialization
+#To set up the twitter API I used the recommened tutorial at https://pythonprogramming.net/twitter-api-streaming-tweets-python-tutorial/
+#The first and third videos of the How to use Twitter API  v1.1 with python to stream tweets were particularly useful
+#As for initializig the client I used the example provided in class (TCP Echo Client). Where I passed in the the following informaion 
+#from the command line: Server IP, Server Port Number, Socket Size, and Hashtag.
+#
+
 import subprocess
 import os
-from keys import ckey,csecret,asecret,atoken
+from keys import ckey,csecret,asecret,atoken #These variables are defined in a seperate file called keys.py (these are the twitter keys)
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
@@ -14,9 +21,8 @@ from tweepy.streaming import StreamListener
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#consumer key, consumer secret, access token, access secret.
 
-# https://gist.github.com/didler/2395703
+# https://gist.github.com/didler/2395703 {
 def getopts(argv):
 	opts ={}
 	while sys.argv:
@@ -44,7 +50,7 @@ if '-s' in myargs:
 if len(myargs.keys()) != 0:
 	print('Invalid arguments')
 	sys.exit()
-
+# }
 
 
 class listener(StreamListener):
@@ -55,6 +61,7 @@ class listener(StreamListener):
 		print('New Tweet: ',tweet,' | User: ',user)
 		tweet = tweet.replace(hashtag,"")
 		parsed = tweet.replace(" ", "_")
+		#ESPEAK
 		cmd_beg = 'espeak -ven+f3 -k5 -s150 '
 		with open(os.devnull, 'w') as devnull:
 			subprocess.run(cmd_beg + parsed, stdout=devnull, stderr=devnull, shell=True)
