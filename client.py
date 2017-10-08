866b958aacc5f445a5b53db2561cae28b679af18
Eric Chandler <chandlr2@vt.edu>
2017-10-07 20:15:05 -0400

    12

--- client.py
+++ client.py
@@ -64,7 +64,6 @@ class listener(StreamListener):
 		print('Sending question: ',tweet)
 		data_ = s.recv(socket_size)
 		s.close()
-		data_ = data.split('b'')[1].split(''')[0]
 		print('Received answer:', data_)
 	def on_error(self, status):
 		print(status)
